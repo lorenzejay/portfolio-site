277e8357a351b3538f26d6b16cbf232426be37ae
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-23 05:59:35 -0800

    nothing chnaged

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from "next/head";
+import About from "../components/about";
 import Hero from "../components/hero";
 import Layout from "../components/layout";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <About />
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
