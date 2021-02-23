d88304adabbbf2679654560ec223b6f789916b06
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-23 07:23:53 -0800

    adjusted sizing for xl screens

--- components/paddingWrapper.js
+++ components/paddingWrapper.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PaddingWrapper = ({ children, className }) => {
-  return <div className={`px-5 lg:px-20 ${className}`}>{children}</div>;
+  return <div className={`px-5 lg:px-20 xl:px-44 ${className}`}>{children}</div>;
 };
 
 export default PaddingWrapper;
