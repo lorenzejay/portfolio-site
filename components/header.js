828edd6a00b7a0049a1dcdb610567c0074307e1f
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-26 16:50:32 -0800

    removed unused

--- components/header.js
+++ components/header.js
@@ -2,67 +2,6 @@ import Link from "next/link";
 import styled from "styled-components";
 import { FaBars } from "react-icons/fa";
 import { motion } from "framer-motion";
-// export const Nav = styled.header`
-//   //handle transitions here
-//   /* background: #fff; */
-
-//   height: 80px;
-//   margin-top: -80px;
-//   display: flex;
-//   align-items: center;
-//   font-size: 1rem;
-//   position: sticky;
-//   top: 0;
-//   z-index: 60;
-// `;
-
-// export const NavContainer = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   height: 80px;
-//   z-index: 1;
-//   width: 100%;
-//   padding: 0 5vw;
-//   @media screen and (min-width: 1024px) {
-//     padding: 0 10vw;
-//   }
-//   /* max-width: 1100px; */
-// `;
-
-// //we are grabbing the link from gatsby link
-// // export const NavLink = styled(LinkG)`
-// //   color: ${props => (props.homePage ? "#fff" : "#000")};
-// //   color: #fff;
-// //   justify-self: flex-start;
-// //   cursor: pointer;
-// //   font-size: 1.5rem;
-// //   display: flex;
-// //   align-items: center;
-// //   margin-left: 6px;
-// //   font-weight: bold;
-// //   text-decoration: none;
-// //   text-transform: uppercase;
-// //   letter-spacing: 5px;
-// //   &:hover {
-// //     text-decoration: none;
-// //     color: #ea5454;
-// //   }
-// // `
-// export const NavLogo = styled(Link)`
-//   color: #000;
-//   text-decoration: none;
-//   display: grid;
-//   margin-top: 40px;
-//   height: 100%;
-//   cursor: pointer;
-//   text-transform: uppercase;
-//   font-size: 32px;
-//   &:hover {
-//     color: #ea5454;
-//     text-decoration: none;
-//   }
-// `;
 
 export const MobileIcon = styled.div`
   display: none;
@@ -95,30 +34,7 @@ export const NavMenu = styled.ul`
   }
 `;
 
-// export const NavItem = styled.li`
-//   height: 80px;
-// `;
-
-// //scroll links
-// export const NavLinks = styled(Link)`
-//   color: #000;
-//   display: flex;
-//   align-items: center;
-//   text-decoration: none;
-//   padding: 0 1rem;
-//   height: 100%;
-//   cursor: pointer;
-//   &:hover {
-//     color: #000;
-//     text-decoration: none;
-//   }
-// `;
-
 const Header = ({ toggle, homePage = false }) => {
-  const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "-100%" },
-  };
   return (
     <header
       className={`flex justify-between w-full items-center t-0 z-10 px-5 lg:px-20 xl:px-44 absolute bg-transparent ${
